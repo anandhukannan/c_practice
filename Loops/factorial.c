378bb090d57b29ce9f2508c27b56c7c1381fa0ad
anandhukannan <161869366+anandhukannan@users.noreply.github.com>
2026-06-23 18:05:52 +0530

    Add factorial program using while loop.c

--- Loops/factorial.c
+++ Loops/factorial.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int a;
+int i=1;
+int fact=1;
+printf("Enter a number:");
+scanf("%d",&a);
+while(i<=a)
+{
+    fact=fact*i;
+    i++;
+}
+printf("%d",fact);
+return 0;
+}
 
